76b6f9deefa83c9d28886434888fa0cc2171c50c
Fardeem Munir <munirfardeem@gmail.com>
2023-05-31 16:35:15 -0500

    Prompts

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ import os
 
 import modal
 
-stub = modal.Stub("smol-developer-v1")
+from prompts import (GENERATE_FILE_STRINGS_SYSTEM_PROMPT,
+                     GENERATE_FILE_SYSTEM_PROMPT, GENERATE_FILE_USER_PROMPT,
+                     SHARED_DEPENDENCIES_SYSTEM_PROMPT)
+
+stub = modal.Stub("ai-intern")
 generatedDir = "generated"
 openai_image = modal.Image.debian_slim().pip_install("openai", "tiktoken")
 openai_model = "gpt-4"  # or 'gpt-3.5-turbo',
@@ -122,41 +126,15 @@ def generate_response(system_prompt, user_prompt, *args):
 @stub.function()
 def generate_file(filename, filepaths_string=None, shared_dependencies=None, prompt=None):
     # call openai api with this prompt
-    system_prompt = f"""
-    You are an AI developer who is trying to write a program that will generate code for the user based on their intent.
-        
-    the app is: {prompt}
-
-    the files we have decided to generate are: {filepaths_string}
-
-    the shared dependencies (like filenames and variable names) we have decided on are: {shared_dependencies}
-    
-    only write valid code for the given filepath and file type, and return only the code.
-    do not add any other explanation, only return valid code for that file type.
-    
-    """
-
-    user_prompt = f"""
-    We have broken up the program into per-file generation. 
-    Now your job is to generate only the code for the file {filename}. 
-    Make sure to have consistent filenames if you reference other files we are also generating.
-    
-    Remember that you must obey 3 things: 
-       - you are generating code for the file {filename}
-       - do not stray from the names of the files and the shared dependencies we have decided on
-       - MOST IMPORTANT OF ALL - the purpose of our app is {prompt} - every line of code you generate must be valid code. Do not include code fences in your response, for example
-    
-    Bad response:
-    ```javascript 
-    console.log("hello world")
-    ```
-    
-    Good response:
-    console.log("hello world")
-    
-    Begin generating the code now.
-
-    """
+    system_prompt = GENERATE_FILE_SYSTEM_PROMPT(
+        prompt=prompt,
+        filepaths_string=filepaths_string,
+        shared_dependencies=shared_dependencies,
+    )
+    user_prompt = GENERATE_FILE_USER_PROMPT(
+        filename=filename,
+        prompt=prompt,
+    )
 
     filecode, cost = generate_response.call(
         system_prompt,
@@ -182,15 +160,7 @@ def generate(prompt: str, should_log: bool, directory: str = generatedDir):
     print("Generating filepaths...")
 
     filepaths_string, cost = generate_response.call(
-        """
-    You are an AI developer who is trying to write a program that will generate code for the user based on their intent.
-
-    When given their intent, create a complete, exhaustive list of filepaths that the user would write to make the program.
-
-    only list the filepaths you would write, and return them as a python list of strings.
-
-    do not add any other explanation, only return a python list of strings.
-    """,
+        GENERATE_FILE_STRINGS_SYSTEM_PROMPT,
         prompt,
     )
 
@@ -219,23 +189,14 @@ def generate(prompt: str, should_log: bool, directory: str = generatedDir):
             shared_dependencies = shared_dependencies_file.read()
 
     print('Figuring out shared dependencies...')
+    system_prompt = SHARED_DEPENDENCIES_SYSTEM_PROMPT(
+        prompt=prompt,
+        filepaths_string=filepaths_string,
+    )
+
     # understand shared dependencies
     shared_dependencies, cost = generate_response.call(
-        """
-    You are an AI developer who is trying to write a program that will generate code for the user based on their intent.
-        
-    In response to the user's prompt:
-
-    ---
-    the app is: {prompt}
-    ---
-    
-    the files we have decided to generate are: {filepaths_string}
-
-    Now that we have a list of files, we need to understand what dependencies they share.
-    Please name and briefly describe what is shared between the files we are generating, including exported variables, data schemas, id names of every DOM elements that javascript functions will use, message names, and function names.
-    Exclusively focus on the names of the shared dependencies, and do not add any other explanation.
-    """,
+        system_prompt,
         prompt,
     )
 
