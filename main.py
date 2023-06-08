d4fb1bf75591be5a9b07ccc06c5116e740f942cb
Fardeem Munir <munirfardeem@gmail.com>
2023-06-08 17:25:44 -0500

    Fix fix

--- main.py
+++ main.py
@@ -1,73 +1,17 @@
 import ast
 import os
+import pathlib
+import sys
 
 import modal
 
+script_path = pathlib.Path(os.path.realpath(__file__)).parent
+sys.path.append(str(script_path))
 
-def GENERATE_FILE_SYSTEM_PROMPT(prompt, filepaths_string, shared_dependencies): return f"""
-You are an AI developer who is trying to write a program that will generate code for the user based on their intent.
-        
-the app is: {prompt}
-
-the files we have decided to generate are: {filepaths_string}
-
-the shared dependencies (like filenames and variable names) we have decided on are: {shared_dependencies}
-    
-only write valid code for the given filepath and file type, and return only the code.
-
-do not add any other explanation, only return valid code for that file type.
-"""
-
-
-def GENERATE_FILE_USER_PROMPT(filename, prompt): return f"""
-We have broken up the program into per-file generation. 
-Now your job is to generate only the code for the file {filename}. 
-Make sure to have consistent filenames if you reference other files we are also generating.
-
-Remember that you must obey 3 things: 
-- you are generating code for the file {filename}
-- do not stray from the names of the files and the shared dependencies we have decided on
-- MOST IMPORTANT OF ALL - the purpose of our app is {prompt} - every line of code you generate must be valid code. Do not include code fences in your response, for example
-
-Bad response:
-```javascript 
-console.log("hello world")
-```
-
-Good response:
-console.log("hello world")
-
-Begin generating the code now.
-"""
-
-
-GENERATE_FILE_STRINGS_SYSTEM_PROMPT = """
-You are an AI developer who is trying to write a program that will generate code for the user based on their intent.
-
-When given their intent, create a complete, exhaustive list of filepaths that the user would write to make the program.
-
-only list the filepaths you would write, and return them as a python list of strings.
-
-do not add any other explanation, only return a python list of strings.
-"""
-
-
-def SHARED_DEPENDENCIES_SYSTEM_PROMPT(prompt, filepaths_string): return f"""
-You are an AI developer who is trying to write a program that will generate code for the user based on their intent.
-        
-In response to the user's prompt:
-
----
-the app is: {prompt}
----
-
-the files we have decided to generate are: {filepaths_string}
-
-Now that we have a list of files, we need to understand what dependencies they share.
-Please name and briefly describe what is shared between the files we are generating, including exported variables, data schemas, id names of every DOM elements that javascript functions will use, message names, and function names.
-Exclusively focus on the names of the shared dependencies, and do not add any other explanation.
-"""
-
+from generate_prompts import (GENERATE_FILE_STRINGS_SYSTEM_PROMPT,
+                              GENERATE_FILE_SYSTEM_PROMPT,
+                              GENERATE_FILE_USER_PROMPT,
+                              SHARED_DEPENDENCIES_SYSTEM_PROMPT)
 
 stub = modal.Stub("ai-intern")
 generatedDir = "generated"
